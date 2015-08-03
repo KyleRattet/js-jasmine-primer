20ec73e3f67458c3027ca7a5a40f1a89fa496674
KyleRattet <krattet@gmail.com>
2015-08-03 13:41:32 -0600

    tax example solution

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-exports.outputHelloWorld = function (word) {
-  return "Hello" + word;
+exports.outputHelloWorld = function () {
+  return "Hello, world!";
 };
+
+exports.calculate = function (amount) {
+  if (amount <= 10) {
+    return amount * .1;
+  } else if (amount <= 20) {
+    return (amount - 10) *.07 + 1;
+  } else if (amount <=30) {
+    return (amount - 20)*.05 + 1 + .70;
+  } else {
+    return (amount - 30) *.03 + 1 + .7 + .5;
+  }
+};
+
+
+
